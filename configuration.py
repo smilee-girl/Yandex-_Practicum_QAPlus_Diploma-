b52ae4f32751fea6def760829c735e72a7f571c2
Ani <anishka7@yandex.ru>
2023-10-01 02:39:18 +0400

    My commit

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://d87063fd-2933-4b98-8690-5db0ca9507be.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://788a6c71-7f77-47cd-b130-dbf84dd6b680.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_INFO_BY_TRACK_PATH = "/api/v1/orders/track"
